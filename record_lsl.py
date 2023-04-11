f83a440d5fd51f1d2bcdcacf501a39834fdfb1b1
Waddaben <wadda@iduntechnologies.com>
2023-04-11 08:55:10 +0200

    added timing choice

--- record_lsl.py
+++ record_lsl.py
@@ -8,7 +8,9 @@ from lsl_utils import stream_data
 
 EXPERIMENT: str = "lsl_stream"
 RECORDING_TIMER: int = 1000000
-LED_SLEEP: bool = False
+LED_SLEEP: bool = True
+SENDING_TIMEOUT: float = 1
+BI_DIRECTIONAL_TIMEOUT: float = 4
 
 # start a recording session
 bci = GuardianClient()
@@ -21,7 +23,11 @@ async def main():
     """
     await asyncio.gather(
         bci.start_recording(
-            recording_timer=RECORDING_TIMER, led_sleep=LED_SLEEP, experiment=EXPERIMENT
+            recording_timer=RECORDING_TIMER,
+            led_sleep=LED_SLEEP,
+            experiment=EXPERIMENT,
+            sending_timout=SENDING_TIMEOUT,
+            bi_directional_receiving_timeout=BI_DIRECTIONAL_TIMEOUT,
         ),
         stream_data(bci.guardian_api),
     )
