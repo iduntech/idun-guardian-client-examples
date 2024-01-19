32a6c626ff732131231b3605f383629f7ca0c224
Abigail Holland <abigailholland22@gmail.com>
2024-01-19 14:15:30 +0100

    added filtering, api key defaults

--- record_lsl.py
+++ record_lsl.py
@@ -13,11 +13,12 @@ SENDING_TIMEOUT: float = 2  # If no receipt is received for 2 seconds, the data 
                            # If you experience excessive disruptions, try increasing this value
 BI_DIRECTIONAL_TIMEOUT: float = 4  # If no bi-directional data is received for 4 seconds, the connection is re-established
                                     # If you experience excessive disruptions, try increasing this value
+FILTER_DATA = False
 
 # start a recording session
 bci = GuardianClient()
 bci.address = asyncio.run(bci.search_device())
-
+#bci.guardian_api.password = "APIKEY"
 
 async def main():
     """
@@ -30,8 +31,9 @@ async def main():
             experiment=EXPERIMENT,
             sending_timout=SENDING_TIMEOUT,
             bi_directional_receiving_timeout=BI_DIRECTIONAL_TIMEOUT,
+            filter_data=FILTER_DATA,
         ),
-        stream_data(bci.guardian_api),
+        stream_data(api_class=bci.guardian_api, filter_data=FILTER_DATA),
     )
 
 
